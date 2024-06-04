880a858261310401d7fd3d9043c57e748d0a6d4e
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-06-04 23:38:16 +0300

    database

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age,sex,color,temperament,alive)
+VALUES ( 'Mr.Chocolate', 13,'M', 'blue','artistic',1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "brown", "gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "stoic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "M", "brown", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "F", "white", "curious", 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (7, "F", "brown", "wild", 1);
